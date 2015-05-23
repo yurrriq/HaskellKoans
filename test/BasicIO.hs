6ea4c268b5b683bd975a0937af71eb624be79529
Eric Bailey <eric@ericb.me>
2015-05-23 16:33:02 -0500

    :lipstick:

--- test/BasicIO.hs
+++ test/BasicIO.hs
@@ -5,31 +5,26 @@ import Test.HUnit (assertBool, assertEqual)
 
 tests :: Spec
 tests = describe "BasicIO" $ do
-    testReadingChar
-    testReadingLine
+  testReadingChar
+  testReadingLine
 
 failIO :: String -> IO a
 failIO fnName =
-    fail $
-      "\n\n\t\x1B[32;1mCheck documentation\x1B[0m of \x1B[33;1m"
-      ++ fnName
-      ++ "\x1B[0m on:\n\t"
-      ++ "http://hackage.haskell.org/packages/archive/base/latest/doc/html/Prelude.html"
+  fail $
+    "\n\n\t\x1B[32;1mCheck documentation\x1B[0m of \x1B[33;1m"
+    ++ fnName
+    ++ "\x1B[0m on:\n\t"
+    ++ "http://hackage.haskell.org/packages/archive/base/latest/doc/html/Prelude.html"
 
 testReadingChar :: Spec
 testReadingChar = it "getChar" $ do
   putStrLn "Write: \"a\" to pass this test: "
   -- NOTE: replace 'failIO' with the actual function
   result <- failIO "getChar"
-  assertEqual ""
-              'a'
-              result
+  assertEqual "" 'a' result
 
 testReadingLine :: Spec
 testReadingLine = it "getLine" $ do
-    -- NOTE: replace 'failIO' with the actual function
-    result <- failIO "getLine"
-    assertEqual "Write: \"burrito\" to pass this test"
-                "burrito"
-                result
-
+  -- NOTE: replace 'failIO' with the actual function
+  result <- failIO "getLine"
+  assertEqual "Write: \"burrito\" to pass this test" "burrito" result
--- test/Lists.hs
+++ test/Lists.hs
@@ -6,91 +6,90 @@ import Util
 
 koans :: Koan
 koans = describe "Lists" $ do
-    koanDrop
-    koanTake
-    koanHead
-    koanTail
-    koanInit
-    koanLast
-    koanConcat
-    koanReplicate
-    koanRepeat
-    koanIterate
+  koanDrop
+  koanTake
+  koanHead
+  koanTail
+  koanInit
+  koanLast
+  koanConcat
+  koanReplicate
+  koanRepeat
+  koanIterate
 
 koanDrop :: Koan
 koanDrop = koan "drop function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "drop"
-    assertEqual "use drop function" [4,5,6,7,8,9,10] result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "drop"
+  assertEqual "use drop function" [4,5,6,7,8,9,10] result
 
 koanTake :: Koan
 koanTake = koan "take function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "take"
-    assertEqual "use take function" [1,2,3] result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "take"
+  assertEqual "use take function" [1,2,3] result
 
 koanHead :: Koan
 koanHead = koan "head function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "head"
-    assertEqual "use head function" 1 result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "head"
+  assertEqual "use head function" 1 result
 
 koanTail :: Koan
 koanTail = koan "tail function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "tail"
-    assertEqual "use tail function" [2,3,4,5,6,7,8,9,10] result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "tail"
+  assertEqual "use tail function" [2,3,4,5,6,7,8,9,10] result
 
 
 koanInit :: Koan
 koanInit = koan "init function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "init"
-    assertEqual "use init function" [1,2,3,4,5,6,7,8,9] result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "init"
+  assertEqual "use init function" [1,2,3,4,5,6,7,8,9] result
 
 koanLast :: Koan
 koanLast = koan "last function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "last"
-    assertEqual "use last function" 10 result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "last"
+  assertEqual "use last function" 10 result
 
 koanConcat :: Koan
 koanConcat = koan "concat function" $ do
-    -- REPLACE: replaceValue with correct function call
-    -- using list [1..10]
-    let result = replaceValue "concat"
-    assertEqual "use concat function" [5,6,1,2,3,4,5,6,7,8,9,10] result
+  -- REPLACE: replaceValue with correct function call
+  -- using list [1..10]
+  let result = replaceValue "concat"
+  assertEqual "use concat function" [5,6,1,2,3,4,5,6,7,8,9,10] result
 
 
 koanReplicate :: Koan
 koanReplicate = koan "replicate function" $ do
-    -- REPLACE: replaceValue with correct function call
-    let result = replaceValue "replicate"
-    assertEqual "use replicate function" [4,4,4,4] result
+  -- REPLACE: replaceValue with correct function call
+  let result = replaceValue "replicate"
+  assertEqual "use replicate function" [4,4,4,4] result
 
 koanRepeat :: Koan
 koanRepeat = koan "repeat function" $ do
-    -- REPLACE: replaceValue with correct function call
-    let result = replaceValue "repeat"
-    -- NOTE: repeat is an infinite function, that's why we need to use the
-    -- take 3. Haskell is a lazy language so it will evaluate only the 3
-    -- first elements of the inifite list, assert would try to eval all the
-    -- last consuming all memory eventually.
-    assertEqual "use repeat function" [1,1,1] (take 3 result)
+  -- REPLACE: replaceValue with correct function call
+  let result = replaceValue "repeat"
+  -- NOTE: repeat is an infinite function, that's why we need to use the
+  -- take 3. Haskell is a lazy language so it will evaluate only the 3
+  -- first elements of the inifite list, assert would try to eval all the
+  -- last consuming all memory eventually.
+  assertEqual "use repeat function" [1,1,1] (take 3 result)
 
 koanIterate :: Koan
 koanIterate = koan "iterate function" $ do
-    -- REPLACE: replaceValue with correct function call
-    let result = replaceValue "iterate"
-    -- NOTE: iterate is an infinite function, that's why we need to use the
-    -- take 3. Haskell is a lazy language so it will evaluate only the 3
-    -- first elements of the inifite list, assert would try to eval all the
-    -- last consuming all memory eventually.
-    assertEqual "use iterate function" [1,2,3,4,5] (take 5 result)
-
+  -- REPLACE: replaceValue with correct function call
+  let result = replaceValue "iterate"
+  -- NOTE: iterate is an infinite function, that's why we need to use the
+  -- take 3. Haskell is a lazy language so it will evaluate only the 3
+  -- first elements of the inifite list, assert would try to eval all the
+  -- last consuming all memory eventually.
+  assertEqual "use iterate function" [1,2,3,4,5] (take 5 result)
